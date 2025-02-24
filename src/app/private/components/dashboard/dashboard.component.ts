b48cba66a6db113d44f148dee04026baa881eb72
AndrewNovik <vitalevich16@gmail.com>
2025-02-24 23:05:40 +0300

    #4 fin example websockets

--- src/app/private/components/dashboard/dashboard.component.ts
+++ src/app/private/components/dashboard/dashboard.component.ts
@@ -1,16 +1,42 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TodoService } from '../../service/todo.service';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
   private readonly todoService: TodoService = inject(TodoService);
 
+  todos$?: Observable<any>;
+
+  form: FormGroup = new FormGroup({
+    title: new FormControl(null, [Validators.required, Validators.email]),
+    content: new FormControl(null, [Validators.required]),
+    f_done: new FormControl(null, [Validators.required]),
+  });
+
   ngOnInit(): void {
-    this.todoService.sendMessage();
+    this.todoService.getTodos();
+    this.todoService.getAddedTodos();
+    this.todos$ = this.todoService.todoItems$;
+  }
+
+  getTodos() {
+    this.todoService.getTodos();
+  }
+
+  addTodo() {
+    this.todoService.saveTodo(this.form.getRawValue());
   }
 }
--- src/app/private/service/todo.service.ts
+++ src/app/private/service/todo.service.ts
@@ -1,22 +1,44 @@
 import { Injectable } from '@angular/core';
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import { tokenGetter } from '../../app.config';
+import { BehaviorSubject } from 'rxjs';
 
+export type TodoItem = {
+  id: number;
+  title: string;
+  content: string;
+  f_done: boolean;
+};
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  constructor() {}
-
-  socket = io('http://localhost:3000/todos', {
+  socket: Socket = io('http://localhost:3000/todos', {
     auth: {
       authorization: tokenGetter(),
     },
   });
+  todoItemsSubject = new BehaviorSubject<TodoItem[]>([]);
+  public todoItems$ = this.todoItemsSubject.asObservable();
 
-  public sendMessage() {
-    this.socket.emit('message', 'message');
+  getTodos() {
+    console.log('try get todos');
 
-    console.log(this.socket.emit('message', 'message'));
+    this.socket.on('todos', (todos: TodoItem[]) => {
+      console.log(todos);
+      this.todoItemsSubject.next(todos);
+    });
+  }
+
+  getAddedTodos() {
+    this.socket.on('addedTodo', (todos) => {
+      console.log(todos);
+      this.todoItemsSubject.next([...this.todoItemsSubject.value, todos]);
+    });
+  }
+
+  saveTodo(todoitem: any) {
+    console.log(todoitem);
+    this.socket.emit('addTodo', todoitem);
   }
 }
